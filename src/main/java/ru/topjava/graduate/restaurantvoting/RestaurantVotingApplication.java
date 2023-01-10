c0839ba138844a2f3944ba05c626b3dab495780b
Sergey <kuznecosa96@mail.ru>
2023-01-10 15:26:11 +0300

    add H2 tcp server and UserRepository

--- src/main/java/ru/topjava/graduate/restaurantvoting/RestaurantVotingApplication.java
+++ src/main/java/ru/topjava/graduate/restaurantvoting/RestaurantVotingApplication.java
@@ -1,13 +1,28 @@
 package ru.topjava.graduate.restaurantvoting;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.topjava.graduate.restaurantvoting.model.Role;
+import ru.topjava.graduate.restaurantvoting.model.User;
+import ru.topjava.graduate.restaurantvoting.repository.UserRepository;
+
+import java.util.Set;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+@AllArgsConstructor
+public class RestaurantVotingApplication  implements ApplicationRunner {
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
     }
 
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(userRepository.findAll());
+    }
 }
